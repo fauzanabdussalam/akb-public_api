d84b72a70503e749a189e798768e3c68c10e78c8
Fauzan M. Abdussalam <fauzan@3trust.com>
2022-07-15 14:00:07 +0700

    initial

---
+++ app/src/main/java/com/example/a10119901_api_ongkir/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a10119901_api_ongkir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
